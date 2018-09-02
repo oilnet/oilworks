dcba5e556c8bdaca87bdafe9257e0463a276097a
J. R. Schmid <jrs+git@weitnahbei.de>
2018-09-02 13:29:24 +0200

    Adding code to setup script for downloading missing default fonts.

--- setup.sh
+++ setup.sh
@@ -7,19 +7,59 @@ echo
 echo "Otherwise, please hit ENTER to confirm and continue."
 read
 
-cd ..
+if [ "$(fc-list | grep 'Charis SIL:style=Regular')" == "" ]; then
+  echo "Charis SIL font not installed. Downloading into oilworks directory."; (
+    wget --quiet -O /tmp/Charis.zip https://software.sil.org/downloads/r/charis/CharisSIL-5.000.zip
+    cd /tmp
+    unzip /tmp/Charis.zip
+  ); (
+    mkdir -p fonts
+    cd fonts 
+    mv /tmp/Charis*/*.ttf .
+    rm -rf /tmp/Charis*
+  )
+fi
 
-echo "What is your project's name? Only lowercase, no spaces, please: "
-read NAME
-cp oilworks/sample.bib $NAME.bib
-cp oilworks/sample.tex $NAME.tex
-ln -s oilworks/lib
-mkdir -p images
-cp oilworks/images/uofl_official_seal.png images/
-cp oilworks/.gitignore .
-cat oilworks/Makefile | sed s/sample/$NAME/g > Makefile
-git add .
-git commit -a -m "Initial commit."
+if [ "$(fc-list | grep 'Scheherazade:style=Regular')" == "" ]; then
+  echo "Scheherazade font not installed. Downloading into oilworks directory."; (
+    wget --quiet -O /tmp/Scheherazade.zip https://software.sil.org/downloads/r/scheherazade/Scheherazade-2.100.zip
+    cd /tmp
+    unzip /tmp/Scheherazade.zip
+  ); (
+    mkdir -p fonts
+    cd fonts 
+    mv /tmp/Scheherazade*/*.ttf .
+    rm -rf /tmp/Scheherazade*
+  )
+fi
 
-echo "All done! Type 'make once' to build $NAME.pdf."
+if [ "$(fc-list | grep 'Andika:style=Regular')" == "" ]; then
+  echo "Andika font not installed. Downloading into oilworks directory."; (
+    wget --quiet -O /tmp/Andika.zip https://software.sil.org/downloads/r/andika/Andika-5.000.zip
+    cd /tmp
+    unzip /tmp/Andika.zip
+  ); (
+    mkdir -p fonts
+    cd fonts 
+    mv /tmp/Andika*/*.ttf .
+    rm -rf /tmp/Andika*
+  )
+fi
+
+(
+  cd ..
+  echo "What is your project's name? Only lowercase, no spaces, please: "
+  read NAME
+  cp oilworks/sample.bib $NAME.bib
+  cp oilworks/sample.tex $NAME.tex
+  ln -s oilworks/lib
+  mkdir -p images
+  cp oilworks/images/uofl_official_seal.png images/
+  cp oilworks/.gitignore .
+  cat oilworks/Makefile | sed s/sample/$NAME/g > Makefile
+  git add .
+  git commit -a -m "Initial commit."
+)
+
+echo "All done! Go up one directory ('cd ..') and type 'make once' to build $NAME.pdf."
 echo "Have a nice day!"
